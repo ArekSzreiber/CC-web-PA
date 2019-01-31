e5497f21dc097a3e60de0cf6c804df00a5138be2
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-01-31 13:45:08 +0100

    query to get details of season

--- data/queries.py
+++ data/queries.py
@@ -1,5 +1,21 @@
-from data import data_manager
+from data import data_manager as data
 
 
 def get_shows():
-    return data_manager.execute_select('SELECT id, title FROM shows;')
+    return data.execute_select('SELECT id, title FROM shows;')
+
+
+def get_season(show_title, season_number):
+    return data.execute_select("""
+        SELECT
+            shows.title,
+            shows.rating,
+            shows.year
+        FROM
+            shows
+        JOIN seasons ON shows.id = seasons.show_id
+        WHERE
+            shows.title = %(title)s
+            AND seasons.season_number = %(season)s
+    """, {'title': show_title,
+          'season': season_number})
