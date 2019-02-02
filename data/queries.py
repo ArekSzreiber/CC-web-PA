0a636dad333002d86e3b124ea7a693fa6a71a80c
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-02-02 20:28:49 +0100

    query for top rated shows, tested

--- data/queries.py
+++ data/queries.py
@@ -1,10 +1,35 @@
-from data import data_manager
+from data import data_manager as data
 
 
 def get_shows():
-    return data_manager.execute_select("""
+    return data.execute_select("""
         SELECT
             id,
             title
         FROM shows;
+    """)
+
+
+def get_most_rated():
+    return data.execute_select("""
+        SELECT
+            DISTINCT shows.title,
+            shows.year,
+            shows.runtime,
+            MAX(genres.name) AS genres,
+            shows.rating,
+            shows.trailer,
+            shows.homepage
+        FROM shows
+        JOIN show_genres ON show_genres.show_id = shows.id
+        JOIN genres ON genres.id = show_genres.genre_id
+        GROUP BY
+            title,
+            year,
+            runtime,
+            rating,
+            trailer,
+            homepage
+        ORDER BY rating DESC
+        LIMIT 15;
     """)
