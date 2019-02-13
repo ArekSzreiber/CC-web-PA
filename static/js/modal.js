d00344fbecc387ceda8f9d04c126aca1e496c6f9
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-02-13 10:40:33 +0100

    episodes modal can be show by click on button

--- static/js/modal.js
+++ static/js/modal.js
@@ -3,3 +3,11 @@ function hideModal(){
 }
 let modalBody = document.getElementById('episodesModalWrapper');
 modalBody.addEventListener('click', hideModal);
+
+function showModal(){
+    document.getElementById('episodesModalWrapper').style.display = 'block';
+}
+
+
+let modalButton = document.getElementById('episodesButton');
+modalButton.addEventListener('click', showModal);
